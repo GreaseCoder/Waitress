be62ee1032ed0b2c479d25df2df0d3d623599cdc
Wes Clawson <wmeadow@gmail.com>
2022-04-22 15:46:16 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Waitress (GreaseCoder Server)
+Waitress is a linux host and this is a collection of scripts used for initial configuration.
+
+## Installation
+Run the scripts, yo
---
+++ server-setup.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install openssh-server
+
+wget https://download.docker.com/linux/static/stable/x86_64/docker-18.03.0-ce.tgz
+tar xzvf docker-18.03.0-ce.tgz
+sudo cp docker/* /usr/bin/
+
+rm docker-18.03.0-ce.tgz
+rm -r docker
+
+sudo dockerd &
+
+
